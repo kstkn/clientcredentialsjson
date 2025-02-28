7d9d691286069aa8c8f8c1740ee4f7c4d9bfad1e
Sergei Kasatkin <490653+kstkn@users.noreply.github.com>
2025-02-28 14:05:10 +0100

    Put back the go authors copyright; Add LICENSE

--- clientcredentials.go
+++ clientcredentials.go
@@ -1,3 +1,9 @@
+// Copyright 2014 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package clientcredentialsjson is an adaptiation of golang.org/x/oauth2/clientcredentials
+// to the scenario where auth server expects credentials in JSON body
 package clientcredentialsjson
 
 import (
